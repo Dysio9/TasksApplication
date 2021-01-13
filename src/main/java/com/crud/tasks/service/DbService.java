166b8844645325f411dc5e9e5b3cd48326d8650d
Dysio9 <dysio9@gmail.com>
2021-01-13 23:59:40 +0100

    19.2 - add DbService.java

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -1,12 +1,21 @@
 package com.crud.tasks.service;
 
+import com.crud.tasks.domain.Task;
 import com.crud.tasks.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
-@RequiredArgsConstructor
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor // tworzy kontstruktor dla wszystkich pól oznaczonych jako final
 public class DbService {
 
-    private final TaskRepository taskRepository;
+    private final TaskRepository repository;
+
+    public List<Task> getAllTasks() {
+        return repository.findAll();
+    }
 
 /*
 ----- Taki sam konstruktor ze wszystkimi parametrami jest tworzony przez @RequiredArgsConstructor -----
