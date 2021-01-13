8de923f16ec6837d734ba18182917eaed244ff52
Dysio9 <dysio9@gmail.com>
2021-01-13 12:57:26 +0100

    19.2 - add method - getTask(Long taskId);

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,28 +1,32 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/v1/task")
+@RequiredArgsConstructor
 public class TaskController {
+
+    private final DbService service;
+    private final TaskMapper taskMapper;
 
     @GetMapping(value = "getTasks")
     public List<TaskDto> getTasks() {
-        return new ArrayList<>();
+        List<Task> tasks = service.getAllTasks();
+        return taskMapper.mapToTaskDtoList(tasks);
     }
 
     @GetMapping(value = "getTask")
     public TaskDto getTask(Long taskId) {
-        return new TaskDto(1L, "test title", "test_content");
-    }
-
-    @DeleteMapping(value = "deleteTask")
-    public void deleteTask(Long taskId) {
-
+        Task task = service.getTask(taskId);
+        return taskMapper.mapToTaskDto(task);
     }
 
     @PutMapping(value = "updateTask")
